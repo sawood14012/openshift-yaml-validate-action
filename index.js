d812675cf6cc4f256303db18d51524f11f98c5d7
Meer Sawood <msawood@redhat.com>
2022-02-22 12:57:51 +0530

    added something new

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const fs = require('fs');
 const core = require('@actions/core');
 const github = require('@actions/github');
+const tc = require('@actions/tool-cache');
 const { exec } = require('child_process');
 
+async function setup() {
+    // Get version of tool to be installed
+  
+    // Download the specific version of the tool, e.g. as a tarball
+    const pathToTarball = await tc.downloadTool(`https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-linux-amd64.tar.gz`);
+  
+    // Extract the tarball onto the runner
+    const pathToCLI = await tc.extractTar(pathToTarball);
+  
+    // Expose the tool by adding it to the PATH
+    core.addPath(pathToCLI)
+}
 
 function getyamlsfromdir(dir){
     try{
@@ -17,6 +30,26 @@ function getyamlsfromdir(dir){
     }
 }
 
+function execute_command(yaml){
+    exec(`oc process --local -f ${yaml} -o yaml > blueprint.yaml`, (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          throw err;
+        }
+        exec(`kubeval blueprint.yaml`, (err, stdout, stderr) => {
+            if (err) {
+                // node couldn't execute the command
+                throw err;
+              }
+              console.log(`stdout: ${stdout}`);
+              console.log(`stderr: ${stderr}`);
+        });
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
+}
+
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -26,16 +59,10 @@ try {
   if(isDir === 'true'){
     var files_found = getyamlsfromdir(yaml_path);
     console.log(`Validating following yamls: ${files_found}`)
-    exec('oc', (err, stdout, stderr) => {
-        if (err) {
-          // node couldn't execute the command
-          return;
-        }
-      
-        // the *entire* stdout and stderr (buffered)
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      });
+    files_found.forEach(function(yaml, index){
+        execute_command(yaml);
+    })
+    
   }
   else{
     console.log(`Validating the yaml from ${yaml_path}`)
