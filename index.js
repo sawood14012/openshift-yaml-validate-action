dc6822a40122eea323a655eca85f8d4596f1a497
Meer Sawood <msawood@redhat.com>
2022-02-23 22:27:29 +0530

    some improvements new

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { exec } = require('child_process');
   if (require.main === module) {
         try {
             // `who-to-greet` input defined in action metadata file
-            const result = {}
             const yaml_path = core.getInput('yaml-path');
             const isDir = core.getInput('is_dir');
             console.log(`Looking for yaml files!`)
@@ -14,19 +13,15 @@ const { exec } = require('child_process');
               var files_found = getyamlsfromdir(yaml_path);
               console.log(`Validating following yamls: ${files_found}`)
               files_found.forEach(function(yaml, index){
-                  execute_command(yaml_path+'/'+yaml).then(function(res){
-                    result = res
-                  });
+                  execute_command(yaml_path+'/'+yaml);
               })
               
             }
             else{
               console.log(`Validating the yaml from ${yaml_path}`)
-              execute_command(yaml_path).then(function(res){
-                result = res
-              });
+              execute_command(yaml_path);
             }
-            core.setOutput("result", result);
+            
             // Get the JSON webhook payload for the event that triggered the workflow
             //const payload = JSON.stringify(github.context.payload, undefined, 2)
             //console.log(`The event payload: ${payload}`);
@@ -49,13 +44,12 @@ function getyamlsfromdir(dir){
 }
 
 function execute_command(yaml){
-    let result = {}
     exec(`oc process --local -f ${yaml} | kubeval --openshift`, (err, stdout, stderr) => {
-        result = {
+        const result = {
           err: err,
           stdout: stdout,
           stderr: stderr
         }
+        core.setOutput("result", result);
       });
-      return result;
 }
